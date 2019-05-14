a8b25e8d7b2ebfd807e3d7a7598f8a05101ed031
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-05-14 10:34:46 -0300

    Criada classe para a tabela 'Gerenciamento'

---
+++ Sistema de Gerênciamento de Militares/Sistema de Gerênciamento de Militares/DataBase/Gerenciamento.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Gerênciamento_de_Militares.DataBase
+{
+    public class Gerenciamento
+    {
+        private readonly MySQL My;
+
+        public Gerenciamento()
+        {
+            My = new MySQL();
+        }
+
+        public bool Insert(int militar, string motivo, DateTime saida, DateTime retorno)
+        {
+            string sql = $@"INSERT INTO gerenciamento
+                               VALUES (DEFAULT
+                                    ,  {militar}
+                                    ,  '{motivo}'
+                                    ,  '{saida.ToString()}'
+                                    ,  '{retorno.ToString()}');";
+
+            return My.ExecuteNonQuery(sql);
+        }
+    }
+}
