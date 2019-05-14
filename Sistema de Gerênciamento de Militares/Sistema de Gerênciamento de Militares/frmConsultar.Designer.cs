10c3d7217e4b3ac774689a73907511b6b5ac28c8
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-05-14 19:04:53 -0300

    Configurado para preencher o ComboBox e o DataGridView

--- Sistema de Gerênciamento de Militares/Sistema de Gerênciamento de Militares/frmConsultar.Designer.cs
+++ Sistema de Gerênciamento de Militares/Sistema de Gerênciamento de Militares/frmConsultar.Designer.cs
@@ -43,74 +43,84 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial Black", 13.8F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(333, 9);
+            this.label2.Location = new System.Drawing.Point(250, 7);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(267, 33);
+            this.label2.Size = new System.Drawing.Size(209, 27);
             this.label2.TabIndex = 3;
             this.label2.Text = "Consultar Militares";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(22, 69);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(16, 56);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.Size = new System.Drawing.Size(49, 15);
             this.label1.TabIndex = 4;
             this.label1.Text = "Nome:";
             // 
             // comboBox4
             // 
+            this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(76, 69);
+            this.comboBox4.Location = new System.Drawing.Point(57, 56);
+            this.comboBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(391, 24);
+            this.comboBox4.Size = new System.Drawing.Size(294, 21);
             this.comboBox4.TabIndex = 15;
             // 
             // button1
             // 
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.Location = new System.Drawing.Point(495, 69);
+            this.button1.Location = new System.Drawing.Point(371, 56);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(138, 40);
+            this.button1.Size = new System.Drawing.Size(104, 32);
             this.button1.TabIndex = 16;
             this.button1.Text = "Pesquisar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.BPesquisar_Click);
             // 
             // button2
             // 
             this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.Location = new System.Drawing.Point(820, 69);
+            this.button2.Location = new System.Drawing.Point(615, 56);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(138, 40);
+            this.button2.Size = new System.Drawing.Size(104, 32);
             this.button2.TabIndex = 17;
             this.button2.Text = "Voltar";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.BVoltar_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(656, 69);
+            this.button3.Location = new System.Drawing.Point(492, 56);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(138, 40);
+            this.button3.Size = new System.Drawing.Size(104, 32);
             this.button3.TabIndex = 18;
             this.button3.Text = "Excluir";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.BExcluir_Click);
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(35, 126);
+            this.dataGridView1.Location = new System.Drawing.Point(26, 102);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(923, 460);
+            this.dataGridView1.Size = new System.Drawing.Size(692, 374);
             this.dataGridView1.TabIndex = 19;
             // 
             // frmConsultar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(986, 611);
+            this.ClientSize = new System.Drawing.Size(740, 496);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -119,6 +129,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmConsultar";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Sistema de Gerenciamento de Militares";
--- Sistema de Gerênciamento de Militares/Sistema de Gerênciamento de Militares/frmConsultar.cs
+++ Sistema de Gerênciamento de Militares/Sistema de Gerênciamento de Militares/frmConsultar.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sistema_de_Gerênciamento_de_Militares.DataBase;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,16 +13,46 @@ namespace Sistema_de_Gerênciamento_de_Militares
 {
     public partial class frmConsultar : Form
     {
+        private readonly Militar militar;
+        private readonly Gerenciamento gerenciamento;
+
         public frmConsultar()
         {
             InitializeComponent();
+
+            militar = new Militar();
+            gerenciamento = new Gerenciamento();
+
+            comboBox4 = militar.CBoxNomeMilitares(comboBox4);
+            PreencherDGV();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        #region Métodos Privados
+
+        private void PreencherDGV()
         {
-            frmTelaInicial TelaInicial = new frmTelaInicial();
-            this.Hide();
-            TelaInicial.ShowDialog();
+            dataGridView1.DataSource = gerenciamento.GetTable();
         }
+
+        #endregion
+
+        #region Botões
+
+        private void BPesquisar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BExcluir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BVoltar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        #endregion
     }
 }
