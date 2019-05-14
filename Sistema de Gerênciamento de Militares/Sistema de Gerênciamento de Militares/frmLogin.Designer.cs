c03ae486b2af1b2423c463c8f9183969fb3773b0
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-05-14 18:39:53 -0300

    Adicionado opção para entrar com enter

--- Sistema de Gerênciamento de Militares/Sistema de Gerênciamento de Militares/frmLogin.Designer.cs
+++ Sistema de Gerênciamento de Militares/Sistema de Gerênciamento de Militares/frmLogin.Designer.cs
@@ -46,9 +46,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(243, 93);
+            this.label1.Location = new System.Drawing.Point(182, 76);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(76, 19);
+            this.label1.Size = new System.Drawing.Size(66, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Usuário:";
             // 
@@ -56,9 +57,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(243, 166);
+            this.label2.Location = new System.Drawing.Point(182, 135);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 19);
+            this.label2.Size = new System.Drawing.Size(56, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "Senha:";
             // 
@@ -66,33 +68,37 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial Black", 14F, System.Drawing.FontStyle.Bold);
-            this.label3.Location = new System.Drawing.Point(23, 9);
+            this.label3.Location = new System.Drawing.Point(17, 7);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(536, 33);
+            this.label3.Size = new System.Drawing.Size(424, 27);
             this.label3.TabIndex = 2;
             this.label3.Text = "Sistema de Gerênciamento de Militares";
             // 
             // txtbUsuarioLogin
             // 
-            this.txtbUsuarioLogin.Location = new System.Drawing.Point(341, 93);
+            this.txtbUsuarioLogin.Location = new System.Drawing.Point(256, 76);
+            this.txtbUsuarioLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtbUsuarioLogin.Name = "txtbUsuarioLogin";
-            this.txtbUsuarioLogin.Size = new System.Drawing.Size(188, 22);
+            this.txtbUsuarioLogin.Size = new System.Drawing.Size(142, 20);
             this.txtbUsuarioLogin.TabIndex = 3;
             // 
             // txtbSenhaLogin
             // 
-            this.txtbSenhaLogin.Location = new System.Drawing.Point(341, 163);
+            this.txtbSenhaLogin.Location = new System.Drawing.Point(256, 132);
+            this.txtbSenhaLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtbSenhaLogin.Name = "txtbSenhaLogin";
             this.txtbSenhaLogin.PasswordChar = '*';
-            this.txtbSenhaLogin.Size = new System.Drawing.Size(188, 22);
+            this.txtbSenhaLogin.Size = new System.Drawing.Size(142, 20);
             this.txtbSenhaLogin.TabIndex = 4;
             // 
             // btnEntrarLogin
             // 
             this.btnEntrarLogin.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnEntrarLogin.Location = new System.Drawing.Point(331, 234);
+            this.btnEntrarLogin.Location = new System.Drawing.Point(248, 190);
+            this.btnEntrarLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnEntrarLogin.Name = "btnEntrarLogin";
-            this.btnEntrarLogin.Size = new System.Drawing.Size(87, 40);
+            this.btnEntrarLogin.Size = new System.Drawing.Size(65, 32);
             this.btnEntrarLogin.TabIndex = 5;
             this.btnEntrarLogin.Text = "Entrar";
             this.btnEntrarLogin.UseVisualStyleBackColor = true;
@@ -101,9 +107,10 @@
             // btnSairLogin
             // 
             this.btnSairLogin.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnSairLogin.Location = new System.Drawing.Point(442, 234);
+            this.btnSairLogin.Location = new System.Drawing.Point(332, 190);
+            this.btnSairLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSairLogin.Name = "btnSairLogin";
-            this.btnSairLogin.Size = new System.Drawing.Size(87, 40);
+            this.btnSairLogin.Size = new System.Drawing.Size(65, 32);
             this.btnSairLogin.TabIndex = 7;
             this.btnSairLogin.Text = "Sair";
             this.btnSairLogin.UseVisualStyleBackColor = true;
@@ -112,9 +119,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(12, 53);
+            this.pictureBox1.Location = new System.Drawing.Point(9, 43);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(225, 338);
+            this.pictureBox1.Size = new System.Drawing.Size(169, 275);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 10;
             this.pictureBox1.TabStop = false;
@@ -123,9 +131,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Bold);
-            this.label4.Location = new System.Drawing.Point(346, 326);
+            this.label4.Location = new System.Drawing.Point(260, 265);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(150, 16);
+            this.label4.Size = new System.Drawing.Size(117, 14);
             this.label4.TabIndex = 11;
             this.label4.Text = "Esqueceu a senha? ";
             // 
@@ -133,17 +142,19 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Bold);
-            this.label5.Location = new System.Drawing.Point(328, 342);
+            this.label5.Location = new System.Drawing.Point(246, 278);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(181, 16);
+            this.label5.Size = new System.Drawing.Size(143, 14);
             this.label5.TabIndex = 12;
             this.label5.Text = "Contate o administrador.";
             // 
             // frmLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AcceptButton = this.btnEntrarLogin;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 428);
+            this.ClientSize = new System.Drawing.Size(450, 348);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.pictureBox1);
@@ -155,6 +166,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Sistema de Gerenciamento de Militares";
